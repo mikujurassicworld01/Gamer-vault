d1198959929cb4af74bb9e741d16e52730a5d05f
mikujurassicworld01 <benjamim.almeida@escola.pr.gov.br>
2026-04-13 17:02:47 -0300

    Seila

--- Scr/Main.jsx
+++ Scr/Main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
-function App() {
-  return <h1>Gamer Vault 🚀</h1>;
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App />
+);
